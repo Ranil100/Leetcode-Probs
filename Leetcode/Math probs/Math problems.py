adca3df5f235c23b199bc6facc57ccd3b3bde4eb
Ranil <166100945+Ranil100@users.noreply.github.com>
2026-05-22 12:31:20 +0530

    Solved 6 Problems

---
+++ Leetcode/Math probs/Math problems.py
@@ -0,0 +1,72 @@
+def rev_num(n):                                    # reverse a num
+    s=0                
+    while n>0:
+        r=n%10
+        s=s*10 + r
+        n=n//10
+
+    print(s)
+
+
+rev_num(147)
+
+
+def prime_num(n):                                 # prime num
+
+    if n <= 1:
+        return "invalid num"
+
+    for i in range(2, n):
+        if n % i == 0:
+            return "no"
+
+    return f"{n}is a prime num"
+
+
+print(prime_num(7))
+
+
+def fibbonacci(n):                                 # fibbonacci
+    a,b=0,1
+    for i in range(n):
+        a,b = b,a+b
+    print(b)
+    
+fibbonacci(5)      
+
+def Second_max(list):                             # second largest num
+    first = second = -99999
+    for i in list:
+        if i>first:
+            second = first
+            first = i
+        elif i>second and i!= first:
+            second = i
+    print (second)
+    
+Second_max([1,2,3,4,5])    
+
+def arr_rev(arr):                                # reverse a array
+    left = 0
+    right = len(arr)-1
+    
+    while left < right:
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+        
+        left+=1
+        right-=1
+    print (arr)
+
+arr_rev([1,2,3,4,5])   
+
+def count_occurences(arr,key):                   # count occurences
+    count =0
+    for i in arr:
+        if i == key:
+            count+=1
+    
+    print(f"{key} occured {count} times")        
+    
+count_occurences([1,2,1,1,3],1) 
